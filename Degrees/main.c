52895ec9aae7e61f53769f716f11d952f2827d88
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-04-08 14:14:13 -0700

    use global var to hold last temp used

--- Degrees/main.c
+++ Degrees/main.c
@@ -9,7 +9,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float lastTemperature;
+
 float fahrenheitFromCelsius(float celsius) {
+  lastTemperature = celsius;
   float fahrenheit = celsius * 1.8 + 32.0;
   printf("%f Celsius is %f Fahrenheit\n", celsius, fahrenheit);
   return fahrenheit;
@@ -20,5 +23,6 @@ int main(int argc, const char * argv[]) {
   float freezingInFahrenheit = fahrenheitFromCelsius(freezingInCelsius);
   
   printf("Water freezes at %f degrees Fahrenheit.\n", freezingInFahrenheit);
+  printf("The last teperature converted was %f.\n", lastTemperature);
   return EXIT_SUCCESS;
 }
