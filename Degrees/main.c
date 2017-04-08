4423a660b26f326397ea9c55c3a820f3a4ee8cc2
kickinbahk@gmail.com <kickinbahk@gmail.com>
2017-04-08 14:11:18 -0700

    calculate fahrenheit from given celsius temp

--- Degrees/main.c
+++ Degrees/main.c
@@ -7,9 +7,18 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+
+float fahrenheitFromCelsius(float celsius) {
+  float fahrenheit = celsius * 1.8 + 32.0;
+  printf("%f Celsius is %f Fahrenheit\n", celsius, fahrenheit);
+  return fahrenheit;
+}
 
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  printf("Hello, World!\n");
-  return 0;
+  float freezingInCelsius = 0;
+  float freezingInFahrenheit = fahrenheitFromCelsius(freezingInCelsius);
+  
+  printf("Water freezes at %f degrees Fahrenheit.\n", freezingInFahrenheit);
+  return EXIT_SUCCESS;
 }
